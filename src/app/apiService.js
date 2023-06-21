c55f701d0034b74780bf2a318eff6f2998f0c777
Kimu Chan <kimuchan@Kimus-MacBook-Air.local>
2023-06-21 20:59:10 +0700

    fix bugs

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://pokemon-be-production.up.railway.app',
 });
 
 apiService.interceptors.request.use(
